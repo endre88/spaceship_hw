2ddcfb11ed225ed961e6a947c9099b1bf61188d0
endre88 <endre1988@freemail.hu>
2023-10-06 15:23:40 +0200

    kész

--- app.js
+++ app.js
@@ -1,1 +1,69 @@
-const spaceship = {};
+const spaceShip = {
+  fuel: 400,
+  Passengers: ["John", "Steve", "Sam", "Danielle"],
+  Shields: true,
+  Speedometer: 0,
+  distance: 0,
+  listPassengers: function () {
+    let passenger = this.Passengers;
+    for (let i = 0; i < passenger.length; i++) {
+      console.log(`Passengers ${i}: ${passenger[i]}`);
+    }
+  },
+  addPassengers: function (newPassenger) {
+    this.Passengers.push(newPassenger);
+    console.log(newPassenger + " was added to the ship");
+  },
+  travel: function (distance) {
+    console.log("Trying to travel " + distance);
+    if (this.fuel == 0) {
+      return console.log("Can't go further, tank is empty");
+    } else {
+      this.fuel = this.fuel - distance / 2;
+      if (this.fuel <= 0) {
+        distance = distance - this.fuel * -2;
+        this.fuel = 0;
+        console.log("Can only travel " + distance);
+        this.Speedometer += distance;
+      } else {
+        this.Speedometer += distance;
+      }
+      if (this.fuel < 30) {
+        this.Shields = false;
+        console.log("Fuel is Low, turning off shields...");
+        console.log("the SpaceShip is at: " + this.Speedometer);
+        console.log("the SpaceShip has: " + this.fuel + " fuel");
+      }
+    }
+  },
+
+  /*if (this.fuel <= 0) {
+      distance = distance - this.fuel / 2;
+      this.fuel = 0;
+      this.Speedometer += distance;
+    } else {
+      this.Speedometer += distance;
+    }
+
+    if (this.fuel <= 0) {
+      distance = distance - this.fuel / 2;
+      this.fuel = 0;
+      this.Speedometer += distance;
+    } else {
+      this.Speedometer += distance;
+    }
+    if (this.fuel < 30) {
+      this.Shields = false;
+      console.log("Fuel is Low, turning off shields...");
+      console.log("the SpaceShip is at: " + this.Speedometer);
+      console.log("the SpaceShip has: " + this.fuel + " fuel");
+    }
+  },*/
+};
+
+spaceShip.listPassengers();
+spaceShip.addPassengers("Lindsay");
+spaceShip.listPassengers();
+spaceShip.travel(750);
+spaceShip.travel(200);
+spaceShip.travel(100);
